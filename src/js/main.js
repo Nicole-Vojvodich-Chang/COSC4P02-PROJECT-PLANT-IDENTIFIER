5372c562bd50b3010a4bcbab0bb9ffb011fe0980
Nicole Vojvodić Chang <146908382+Nicole-Vojvodich-Chang@users.noreply.github.com>
2024-02-21 16:29:16 -0500

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+import ToDoList from "/todolist.js";
+import ToDoItem from "/toDoItem.js";
+
+const toDoList = new ToDoList();
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    refreshThePage();
+};
+
+const refreshThePage = () => {
+    clearListDisplay();
+    renderList();
+
+};
+
+const clearListDisplay = () => {
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+};
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while(child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () => {
+    const list = toDoList.getList();
+    list.forEach((item) => {
+        buildListItem(item);
+    });
+};
+
+const buildListItem = (item) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+
+}
